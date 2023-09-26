2b1096ce91e51f00f990a47420607b4a5582fa7e
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-09-26 16:38:33 -0300

    feat: product model and dtos

---
+++ src/main/java/com/sdeli/deliveryapi/dto/input/ProductInput.java
@@ -0,0 +1,30 @@
+package com.sdeli.deliveryapi.dto.input;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class ProductInput {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal price;
+
+    @Valid
+    @NotNull
+    private RestaurantReference restaurant;
+
+}
