b499fadba897498c8a570d3f21d238c4a3b1de2e
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-10-05 17:44:41 -0300

    feat: configure shallow etag

--- src/main/java/com/sdeli/deliveryapi/core/web/WebConfig.java
+++ src/main/java/com/sdeli/deliveryapi/core/web/WebConfig.java
@@ -1,6 +1,9 @@
 package com.sdeli.deliveryapi.core.web;
 
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods("*");
     }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
 }
